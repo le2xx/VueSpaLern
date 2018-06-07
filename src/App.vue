c17191392c601d2ae1d2fadf756bf9d84e2431e3
le2xx <le2x@rambler.ru>
2018-06-07 16:58:24 +0300

    app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <header class="header">
+      <h1>{{ msg }}</h1>
+    </header>
+    <div class="sidebar">
+      <div class="menu">
+      <div class="menu menu__item" v-for="item in menu"
+        :key="item.example">
+        <div class="menu menu__item menu__item_num">{{ item.example }}</div>
+        <div class="menu menu__item menu__item_text">{{ item.text }}</div>
+      </div>
+      </div>
+    </div>
+    <div class="content">
+      <p>content</p>
+    </div>
   </div>
 </template>
 
@@ -24,37 +23,160 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Учимся Vue а за одно и css с js',
+      menu: [
+        { example: 1, text: 'Hello World!'},
+        { example: 2, text: 'Динамически связанные свойства'},
+        { example: 3, text: 'Условия v-if'},
+        { example: 4, text: 'Циклы v-for'},
+        { example: 5, text: 'События v-on'},
+        { example: 6, text: 'Cвязывание элементов форм v-model'}
+      ]
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background-color: #deecde;
+  margin: 0 0 0 0;
+  font-family: "Comic Sans MS", "Comic Sans", cursive;
+}
+
+.header {
+  border-bottom: 2px solid #8c9995;
+  border-bottom-style: dashed;
+  color: green;
+  font-family: "Comic Sans MS", "Comic Sans", cursive;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  min-height: 90px;
 }
 
-h1, h2 {
-  font-weight: normal;
+.header_h2 {
+  color: darkgreen;
+  text-align: left
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.sidebar {
+  width: 20%;
+  padding: 10px 0;
+  box-sizing: border-box;
+  border-right: 2px solid #8c9995;
+  border-right-style: dashed;
+  position: relative;
+  min-height: 100%;
+  top: 90px;
+  margin: 0px 0px 0px 10px
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.menu {
+  color: green;
 }
 
-a {
-  color: #42b983;
+.menu__item {
+  float: none;
+  margin:  5px 0 5px 0;
+  
+}
+
+.menu__item_num {
+  width: 40px;
+  height: 40px;
+  text-align: center;
+  font-size: 20px;
+  line-height: 40px;
+  background: silver;
+  color: #a42f2f;
+  border-radius: 50%;
+  float: left;
+}
+
+.menu__item_text {
+  width: 340px;
+  position: relative;
+  line-height: 50px;
+}
+
+.content {
+  position: fixed;
+  top: 90px;
+  left: 21%;
+  width: 79%;
+  padding: 10px 5px
+}
+
+.example {
+  font-family: "Comic Sans MS", "Comic Sans", cursive
+}
+
+.btn {
+  background-color: #3d6c44;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, .5);
+  color: white;
+  font-family: "Comic Sans MS", "Comic Sans", cursive;
+  margin: 5px;
+  padding: 10px 5px
+}
+
+.btn_remov {
+  background-color: #a42f2f;
+  padding: 2px 5px
+}
+
+.input {
+  background-color: #416f72;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, .5);
+  color: white;
+  font-family: "Comic Sans MS", "Comic Sans", cursive;
+  margin: 5px;
+  padding: 10px 5px
+}
+
+.input::-webkit-input-placeholder {
+  color: white
+}
+
+.table {
+  border: 1px solid black;
+  border-collapse: collapse;
+  counter-reset: schetchik;
+  padding: 0 10px
+}
+
+.table__col {
+  border: 1px solid black;
+  padding: 0 10px
+}
+
+.table__col_head {
+  font-weight: 600
+}
+
+.table .num {
+  counter-increment: schetchik
+}
+
+.table .num:before {
+  content: counter(schetchik)
+}
+
+.select {
+  background-color: #3d6c44;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, .5);
+  color: white;
+  font-family: "Comic Sans MS", "Comic Sans", cursive;
+  margin: 5px;
+  padding: 10px 5px
 }
 </style>
